5b2ac4af2a57ad16794adb975cf6ff9ba064336e
Eddie Morales <eddie@zipcoders-MacBook-Pro.local>
2019-10-26 19:51:48 -0400

    Playlist

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,20 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        int counterForward = 0;
+        int counterBackward = 0;
+        for (int i = 0; i <= startIndex; i++) {
+            counterForward++;
+        }
+        for (int j = 0; j < playList.length; j++) {
+            counterBackward++;
+
+        }
+        if(counterForward < counterBackward){
+            return counterForward;
+        }else {
+
+            return counterBackward;
+        }
     }
 }
